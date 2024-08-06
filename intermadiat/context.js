ace58a440d0f5c937bcd4a2c7cdd716fa1b8a5e3
Tubanoor2007 <tubaq343>
2024-08-06 17:10:53 +0530

    msg

---
+++ intermadiat/context.js
@@ -0,0 +1,16 @@
+// context tutorial
+
+// 0
+//   class contextObj2 {
+//     constructor(a) {
+//       this.a = a;
+//     }
+  
+//     invokeThisObj2() {
+//       return this;
+//     }
+//   }
+//   var inst = new contextObj2(1);
+//   console.log(inst.invokeThisObj2());
+//   var inst2 = new contextObj2(5);
+//   console.log(inst2.invokeThisObj2());
---
+++ intermadiat/thiskeyword.js
@@ -0,0 +1,26 @@
+// 1.alone this refers to the global object 
+// console.log(this);
+// // global object == refer to window object
+
+// // 2.In regular function, this refers to the global object 
+// function sum(){
+//     var add = 2+2;
+//     console.log("sum of two no is "+add);
+//     console.log(this);
+// }
+
+// sum();
+
+// // 3.In a method, this refers to the owner object
+// const aquib = {
+//   name : "aquib naqwa",
+//   qualif : "Bcom",
+//   sum: function(){
+//     var add = 2+2;
+//     console.log("sum of two no is "+add);
+//     console.log(this);
+// }
+// }
+
+
+// aquib.sum();
